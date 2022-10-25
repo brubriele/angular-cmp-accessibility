fd845fa3d3940c06bf274bc9bea3c38ea0efa968
brubriele <brunagabriele.paula@gmail.com>
2022-10-25 16:53:46 -0300

    :wheel_chair: Implementa regra de interação para radio btn, diretiva Keyboard Manager, diretivas de marcação a partir de ContentChildren, closes #7

--- src/app/shared/components/yes-or-no-button-group/yes-or-no-button-group.module.ts
+++ src/app/shared/components/yes-or-no-button-group/yes-or-no-button-group.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YesOrNoButtonGroupComponent } from './yes-or-no-button-group.component';
+import { KeyboardManagerModule } from '../../directives/keyboard-manager/keyboard-manager.module';
 
 
 
 @NgModule({
   declarations: [YesOrNoButtonGroupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    KeyboardManagerModule
   ],
   exports: [YesOrNoButtonGroupComponent]
 })
